eb1c556c1e63c8fba5dca987b3fc93fe0d4909ef
karol-121 <36259145+karol-121@users.noreply.github.com>
2022-02-21 18:36:33 +0100

    eliminated false positives (hopefully)

--- src/mapViewer.js
+++ src/mapViewer.js
@@ -1,46 +1,48 @@
+console.log("finn location lookup is loaded!"); //debug string to dobel check if extension is loading
 
-//wrapper that wraps array object. This allow to "overwrite/extend" the push method to include check
-class ArrayWrapper {
-    #array = new Array(); 
-
-    RegisterDOM(DOMclass, DOMtag) { //method that adds element if it has given classname and tagname
-
-        for (const elem of document.getElementsByClassName(DOMclass)) {
-
-            if (elem.tagName === DOMtag) {
-
-                this.#array.push(elem);
-
-            }
-        }
-      
+//prototype of object that holds relation between category and reference to DOM
+class AddressDOM {
+    constructor(category, path) {
+        this.category = category;
+        this.path = path;
     }
-
-    GetArray() {   //getter for array so it can be read from elsewhere
-        console.log(this.#array);
-        return this.#array;
-    }
-
-    
 }
 
 
+const addressDOMs = new Array(); //collection that holds all registered DOMs
 const url = "https://www.openstreetmap.org/search?query=";   //URL with serach parameter, used to show location in this case on openstreetmap.
 
-const a = new ArrayWrapper();
 
-//definitions of addres text locations for different pages, this creates a lot of false positives
-a.RegisterDOM("u-mb0", "H3");   //address object for "torget" listings
-a.RegisterDOM("u-mh16", "SPAN");   //address object for "bil og næring og boliger" listings
-a.RegisterDOM("u-mh16", "P"); //address object for i guess some "boliger"
-a.RegisterDOM("u-t3", "H2");    //address object for "båter og jobb" listings
+//registering all possible locations of addres for each category
+addressDOMs.push(new AddressDOM("car", document.getElementsByClassName("u-mh16")[1]));
+addressDOMs.push(new AddressDOM("bap", document.getElementsByClassName("u-mb0")[0])); //torget
+addressDOMs.push(new AddressDOM("realestate", document.getElementsByClassName("u-mh16")[1]));
+addressDOMs.push(new AddressDOM("mc"), document.getElementsByClassName('u-t3')[4]);
 
 
-for (const DOM of a.GetArray()) {
+const location = document.location.pathname; //gets pathname of current page
+const subcategory = location.split('/')[1]; //extract category from pathname (url)
+
+//apply address change for each element(location) that match current category
+for (const DOM of addressDOMs) {
+    if (DOM.category === subcategory) {
+        console.log(DOM);
+        changeAddress(DOM.path);
+    }
+}
+
+
+//function that adds link to provided DOM
+function changeAddress(DOM) {
+
+    console.log(DOM.innerHTML);
+
     const address_string = DOM.innerText; //getting addres string from HTML object
     const final_url = url + address_string; //modifing url by adding address string to it.
 
     DOM.innerHTML = "<a href='"+final_url+"' target='_blank'>"+address_string+"</a>"; //replace location text with link to map
 }
+
+
 
 
