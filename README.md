6de99e965cf606c9ce40dbbec99467412e2a2c8d
karol-121 <36259145+karol-121@users.noreply.github.com>
2022-03-17 18:57:09 +0100

    futher work on new extension, modifing repo

--- README.md
+++ README.md
@@ -1,26 +1,45 @@
-![icon](/src/icons/icon-48.png) 
-# Finn.no external map
-Web browser extension for Firefox that lets you roughly lookup listing address on finn.no without having to log in. 
-The extension opens external map service (openstreetmap) with the aproximate location.
+# Finn.no browser extension suite
+Pack of browser extension made for finn.no website. Purpose of these extensions is to make finn experience better. They add functionalites that I personally find lacking. 
+Currently only for firefox, support for other browsers may or may not come.
+
+## Description of extensions found in this suite
+
+### 1. Finn.no - external map
+![icon](/mapViewer/icons/icon-48.png)
+Extension that lets you roughly lookup listing address on finn.no without having to log in. The extension opens external map service (openstreetmap) with the aproximate location.
 This is usefull when you want to see where given address is located on a map without having to log in or you don't want to use finn's own maps.
 
-# How it works?
+#### How it works?
 Adds link to an external map service in place where aproximate addres usually is listed. 
 The external map opens in new tab showing results for that particular location.
 
-# Installation
-1. Download extension located in *release* section to the right.
-2. Refer to this instruction on how to install add-ons from a file: https://support.mozilla.org/en-US/kb/find-and-install-add-ons-add-features-to-firefox
-
-# Known issues
+#### Known issues
 - Map Links generated for job and motorcycle listings are not always correct.
 - Some sections may not have links generated.
 
-# Possible future features
+#### Possible future features
 - Add pin to location on external map.
 - Allow for choosing prefeable map service.
 
-# Development
-Extension soruce files with `manifest.json` file are located in `src` directory. On how to develop extensions refer to this guide: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_first_WebExtension
+### 2. Finn.no - thumbnail slideshow
+![icon](/thumbnailSlideshow/icons/icon-48.png) 
+Extension that implements image slideshow on listing's thumbnail. This make it possible to see listing's item from other perspectives (as long the author has provided these) directly from the frontpage.
+
+#### How it works?
+After hovering on given listing for x time, the thumbnail changes periodicaly showing all listings images in a loop. 
+
+#### Known issues
+- slideshow does now work on all listings, currently only for car like listings.
+
+#### Possible future features
+- none for now
+
+## Installation
+*for now you cannot install extensions as I'm unable to pack and sign them.*
+1. Download extension located in *release* section to the right.
+2. Refer to this instruction on how to install add-ons from a file: https://support.mozilla.org/en-US/kb/find-and-install-add-ons-add-features-to-firefox
+
+## Development
+Extension soruce files with `manifest.json` file are located in their directory (`mapViewer/` for external map extension and `thumbnailSlideshow/` for thumbnail slideshow extension). On how to develop extensions refer to this guide: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_first_WebExtension
 
 
--- thumbnailSlideshow/thumbnailSlideshow.js
+++ thumbnailSlideshow/thumbnailSlideshow.js
@@ -1,16 +1,17 @@
 console.log("thumbnail slideshow extension has been loaded"); //debug string to dobel check if extension is loading
 
-const actionDelay = 1500; //defines the time after which the slideshow starts showing
+const actionDelay = 700; //defines the time after which the slideshow starts showing
+const actionInterval = 1000; //defines how ofter images should change in slideshow
 const galleryUrl = 'https://www.finn.no/gallery.html?finnkode='; //url to gallery from which images will be fetched
+const urlRegEx = /https:\/\/images\.finncdn.no\/.* [a-z0-9]*/gm; //regex that filters srcsets from page source code
 const xhttp = new XMLHttpRequest(); //xhr request object
 
 let currentListing; //holds current object that is being processed (where the slideshow should be shown) 
 let currentListingID; //id of relevant listing, is needed to get images for this specific listing (current target)
 let orginalListingSrcset; //holds the currents object orginal srcset, so it is possible to revert to orgnial state after "mouseout"
 let modified = false; //defines if object is currently modified or not, false here means that no object needs any treatning
-let imageFetchTimeout; //holds timeout for function that will fetch images via xhr. this allows to cancel it before it happens by refering to this variable
-
-const dummyImgUrl = 'https://images.finncdn.no/dynamic/480w/2022/3/vertical-0/16/4/251/444/864_1099160377.jpg'; //for tests
+let imageFetchTimeout; //holds timeout object for function that will fetch images via xhr. this allows to cancel it before it happens by refering to this variable
+let slideshowInterval; //holds interval object for function that will change images.
 
 
 document.addEventListener("mouseover", beginSlideshow);
@@ -22,6 +23,7 @@ function beginSlideshow(event) {
         //in practice this block will only run if previous event started the "modifing process"
 
         clearTimeout(imageFetchTimeout);
+        clearInterval(slideshowInterval);
         currentListing.parentNode.parentNode.previousSibling.firstChild.firstChild.srcset = orginalListingSrcset; //revert modifications
         modified = false; //update status
         console.log("recovered / canceled modifing process");
@@ -50,21 +52,40 @@ function dispatchImgFetch() {
     //this function should only execute if there is need for fetched images.
 
     console.log("dipatched img fetch")
-    xhttp.open("GET", "https://www.finn.no/gallery.html?finnkode="+currentListingID, true);
+    xhttp.open("GET", "https://www.finn.no/gallery.html?finnkode="+currentListingID, true); //todo: use the global defined url
     xhttp.send();
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4) {
 
-            //returns string that contains links to images
-            console.log(xhttp.responseText);
-
-            //update thumbnail with dummy img for now
-            currentListing.parentNode.parentNode.previousSibling.firstChild.firstChild.srcset = dummyImgUrl;
-
-
+            console.log("pass fetched srcsets futher");
+            //passes array with srcsets to function that will show slideshow
+            startSlideshow(xhttp.responseText.match(urlRegEx));
         }
     }
 }
+
+
+function startSlideshow(srcsets) {
+
+    //todo: add check if there is more than one image
+    
+    let i = 1;
+
+    //instatly show the second image as the user has allerady waited for x time to see other images
+    currentListing.parentNode.parentNode.previousSibling.firstChild.firstChild.srcset = srcsets[i];
+
+
+    slideshowInterval = setInterval(function () {
+        //show rest of the images by cycling through the provided array
+
+        i = (i + 1) % srcsets.length;
+
+        currentListing.parentNode.parentNode.previousSibling.firstChild.firstChild.srcset = srcsets[i];
+
+    }, actionInterval)
+
+}
+
 
 
 
