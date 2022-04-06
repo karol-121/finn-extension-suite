b4b1ae7e9596c65506a94d3cdeea62d85f59ecdc
karol-121 <36259145+karol-121@users.noreply.github.com>
2022-04-06 19:22:05 +0200

    new target finding method
    Now subset of provided targets is checked for string with numbers, this still does render fail (mc & boats) but it is more robust now

--- mapViewer/mapViewer.js
+++ mapViewer/mapViewer.js
@@ -1,47 +1,57 @@
 console.log("finn location lookup is loaded!"); //debug string to dobel check if extension is loading
 
 //prototype of object that holds relation between category and reference to DOM
-class AddressDOM {
+class AddressObject {
     constructor(category, path) {
         this.category = category;
         this.path = path;
     }
 }
 
-
-const addressDOMs = new Array(); //collection that holds all registered DOMs
+const targets = new Array(); //collection that holds all registered DOMs
 const url = "https://www.openstreetmap.org/search?query=";   //URL with serach parameter, used to show location in this case on openstreetmap.
+// const url = "https://duckduckgo.com/?iaxm=maps&q=";
+const addressRegEx = /.*\d+.*/gm; //matches strings that have numbers in it
 
 
 //registering all possible locations of addres for each category
-addressDOMs.push(new AddressDOM("car", document.getElementsByClassName("u-mh16")[1]));
-addressDOMs.push(new AddressDOM("bap", document.getElementsByClassName("u-mb0")[0])); //torget
-addressDOMs.push(new AddressDOM("realestate", document.getElementsByClassName("u-mh16")[1]));
-addressDOMs.push(new AddressDOM("mc", document.getElementsByClassName('u-t3')[3]));
-addressDOMs.push(new AddressDOM("job", document.getElementsByClassName('u-t3')[3]));
+targets.push(new AddressObject("car", document.getElementsByClassName("u-mh16")));
+targets.push(new AddressObject("bap", document.getElementsByClassName("u-mb0"))); //torget
+targets.push(new AddressObject("realestate", document.getElementsByClassName("u-mh16")));
+targets.push(new AddressObject("mc", document.getElementsByClassName('u-t3')));
+targets.push(new AddressObject("boat", document.getElementsByClassName('u-t3')));
+targets.push(new AddressObject("job", document.getElementsByClassName('u-t3')));
 
 
 const location = document.location.pathname; //gets pathname of current page
 const subcategory = location.split('/')[1]; //extract category from pathname (url)
 
-//apply address change for each element(location) that match current category
-for (const DOM of addressDOMs) {
+//find desired object/objects to modify
+for (const DOM of targets) {
     if (DOM.category === subcategory) {
-        console.log(DOM);
-        changeAddress(DOM.path);
+
+        //iterate subset of objects assigned to current category
+        for (const a of DOM.path) {
+
+            //only modify those with mathing text 
+            if (a.innerText.match(addressRegEx)) {
+
+                changeAddress(a);
+
+            }
+        }
+
     }
 }
 
 
-//function that adds link to provided DOM
-function changeAddress(DOM) {
+//function that adds link to target object
+function changeAddress(Object) {
 
-    console.log(DOM.innerHTML);
-
-    const address_string = DOM.innerText; //getting addres string from HTML object
+    const address_string = Object.innerText; //getting addres string from HTML object
     const final_url = url + address_string; //modifing url by adding address string to it.
 
-    DOM.innerHTML = "<a href='"+final_url+"' target='_blank'>"+address_string+"</a>"; //replace location text with link to map
+    Object.innerHTML = "<a href='"+final_url+"' target='_blank'>"+address_string+" ↗</a>"; //replace location text with link to map
 }
 
 
