80d276879f5b5464e7254a4f8a1aaf8ddae84569
karol-121 <36259145+karol-121@users.noreply.github.com>
2022-09-15 20:56:00 +0200

    *fixed problem with first listing

--- squareMeters/squareMeters.js
+++ squareMeters/squareMeters.js
@@ -22,6 +22,7 @@ const callback = () => {
   if (url !== lastUrl) {
     lastUrl = url;
     modifyDOM();
+    
   }
 }
 
@@ -38,39 +39,50 @@ function modifyDOM() {
   listings = document.querySelectorAll("article");
 
   //iterate through all listings and add ratio value for each
+  //here the first listing is skipped as it is updated and not replaced, thus modifications are not reset and adds up
   for (listing of listings) {
-    // console.log(listing);
-
-    //div where needed for calculation values exists
-    const dataDiv = listing.children[3].children[2];
-
-    //check if there is atleast two values, otherwise it is not possible to calculate the value
-    if (dataDiv.children.length === 2) {
-
-      //extract kr and m2 value from listing
-      const m2 = extractNumber(dataDiv.children[0].innerText);
-      const kr = extractNumber(dataDiv.children[1].innerText);
-
-      const krPerM2 = calculate(m2,kr);
-
-      //if calculated ratio value is below value, mark listing
-      if(krPerM2 < threshold) {
-        dataDiv.setAttribute("style","color: #d5840b;");
-      }
-
-      //create new span element in which calculated value will be shown
-      const dataSpan = document.createElement("span");
-        dataSpan.append(krPerM2 + " kr/m²");
-
-      //append new span element to listing
-      dataDiv.append(dataSpan);
-    }
-
+    modifyListing(listing);
   }
 
 }
 
+function modifyListing(listing) {
 
+  console.log(listing);
+
+  //div where needed for calculation values exists
+  const dataDiv = listing.children[3].children[2];
+
+  //because the first listings is updated and not replaced, the previous modification will not be removed
+  if(dataDiv.children.length === 3) {
+    dataDiv.children[0].remove();
+  }
+
+  //check if there is atleast two values, otherwise it is not possible to calculate the value
+  if (dataDiv.children.length === 2) {
+
+    //extract kr and m2 value from listing
+    const m2 = extractNumber(dataDiv.children[0].innerText);
+    const kr = extractNumber(dataDiv.children[1].innerText);
+
+    const krPerM2 = calculate(m2,kr);
+
+    //if calculated ratio value is below value, mark listing
+    if(krPerM2 < threshold) {
+      dataDiv.setAttribute("style","color: #d5840b;");
+    } else {
+      dataDiv.removeAttribute("style"); //safe as removeAttribute does not return error if attribute is not found
+    }
+
+    //create new span element in which calculated value will be shown
+    const dataSpan = document.createElement("span");
+      dataSpan.append(krPerM2 + " kr/m²");
+
+    //append new span element to listing
+    dataDiv.append(dataSpan);
+  }
+
+}
 
 
 //function that extract int from a string
