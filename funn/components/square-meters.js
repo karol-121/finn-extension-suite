450ed7247970889c66180a2974a7b46ad7430f03
karol-121 <36259145+karol-121@users.noreply.github.com>
2023-05-16 11:04:54 +0200

    fixed bugs related to square meters component
    Now it does work including the first listing

--- funn/components/square-meters.js
+++ funn/components/square-meters.js
@@ -12,38 +12,69 @@ const square_meters = {
   },
 
   apply() {
-    console.log("square-meters deployed");
-    //collection of listings
     let listings;
 
-    //update listing with current feed
-    listings = document.querySelectorAll("article");
+    //obtain list of currently visible listings
+    listings = document.querySelector('main').children[2].children[1].children[0].children[2].children; 
+
+    //check if list of listings is actually obtained, if not use alternative path
+    //this is necessary as the path to relevant DOM object does change upon i.e adding filters (selected filters are prepended to the page which changes the DOM tree)
+    if (listings.length < 1) {
+      listings = document.querySelector('main').children[2].children[1].children[0].children[3].children;
+    }
 
     //iterate through all listings and add ratio value for each
-    //here the first listing is skipped as it is updated and not replaced, thus modifications are not reset and adds up
     for (listing of listings) {
-      this.modifyListing(listing, this.threshold);
+      this.modifyListing(listing);
     }
   },
 
-  modifyListing(listing, threshold) {
-    //div where needed for calculation values exists
-    const dataDiv = listing.children[3].children[3];
+  modifyListing(listing) {
+
+    //the first listing is different from the rest
+    //therefore handle it accordingly
+    if (listing.tagName === "ARTICLE") {
+      this.modifyContent(listing.children[3].children[2]);
+      return;
+    }
+
+    //check if listing has desired amount of children / expected DOM tree
+    //this way we can assume that we are dealing with the right object
+    //and avoid "undefined" exception when we actually want to access specified object
+
+    //check target's child count
+    if (listing.childElementCount < 4) {
+      return;
+    }
+
+    //check target's child child count
+    if (listing.children[4].childElementCount < 3) {
+      return;
+    }
+
+    //at this point we know that desired object does actually exist so we can safety access it
+    //note that we still does not know if it is the right object, we can only assume it
+    this.modifyContent(listing.children[4].children[3]);
+    
+  },
+
+  modifyContent(target) {
 
     //check if there is two values, otherwise is not possible to calculate the value
-    if (dataDiv.children.length === 2) {
+    //this does also further convince us that we are dealing with expected object
+    if (target.children.length === 2) {
 
       //extract kr and m2 value from listing
-      const m2 = this.extractNumber(dataDiv.children[0].innerText);
-      const kr = this.extractNumber(dataDiv.children[1].innerText);
+      const m2 = this.extractNumber(target.children[0].innerText);
+      const kr = this.extractNumber(target.children[1].innerText);
 
       const krPerM2 = this.calculate(m2,kr);
 
       //if calculated ratio value is below value, mark listing
-      if(krPerM2 < threshold) {
-        dataDiv.setAttribute("style","color: #d5840b;");
+      if (krPerM2 < this.prefs.threshold) {
+        target.setAttribute("style","color: #d5840b;");
       } else {
-        dataDiv.removeAttribute("style"); //safe as removeAttribute does not return error if attribute is not found
+        target.removeAttribute("style"); //safe as removeAttribute does not return error if attribute is not found
       }
 
       //create new span element in which calculated value will be shown
@@ -51,8 +82,9 @@ const square_meters = {
         dataSpan.append(krPerM2 + " kr/m²");
 
       //append new span element to listing
-      dataDiv.append(dataSpan);
+      target.append(dataSpan);
     }
+
   },
 
   //function that extract int from a string
